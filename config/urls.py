bc3b9f0cea5e999102b0e6d5e96784d72b9abc80
Denys Seliverstov <catsperr322@gmail.com>
2023-08-14 21:50:27 +0300

    Changing pathes in urls

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("book.urls", namespace="book")),
+    path("api/v1/books/", include("book.urls", namespace="book")),
     path("api/v1/users/", include("user.urls", namespace="user")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
