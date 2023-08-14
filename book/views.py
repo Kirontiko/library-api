dc986a13e8dccb667e407196e75380ad451e2076
Eugene Polunin <altahoma@gmail.com>
2023-08-14 17:07:13 +0300

    Created view with permission

--- book/views.py
+++ book/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins, status
 
-# Create your views here.
+from book.serializers import (
+    BookSerializer,
+    BookListSerializer,
+    BookDetailSerializer,
+    BookImageSerializer,
+)
+from book.models import Book
+from book.permissions import IsAdminOrIfAuthenticatedReadOnly
+
+
+class BookViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet,
+):
+    queryset = Book.objects.all()
+    permission_classes = IsAdminOrIfAuthenticatedReadOnly
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+
+        if self.action == "retrieve":
+            return BookDetailSerializer
+
+        if self.action == "upload_image":
+            return BookImageSerializer
+
+        return BookSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser],
+    )
+    def upload_image(self, request, pk=None):
+        book = self.get_object()
+        serializer = self.get_serializer(book, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
