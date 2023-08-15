69c5eb94a79b847a722a9d28570a2103deafbb47
Denys Seliverstov <catsperr322@gmail.com>
2023-08-15 16:28:03 +0300

    Adding models to payment service

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from borrowing.models import Borrowing
+
+
+class Payment(models.Model):
+    STATUS_CHOICES = [
+        ("PENDING", "Pending"),
+        ("PAID", "Paid"),
+    ]
+    TYPE_CHOICES = [
+        ("PAYMENT", "Payment"),
+        ("FINE", "Fine"),
+    ]
+    status = models.CharField(choices=STATUS_CHOICES,
+                              max_length=7)
+    type = models.CharField(choices=TYPE_CHOICES,
+                            max_length=7)
+    borrowing_id = models.ForeignKey(Borrowing,
+                                     related_name="payments",
+                                     on_delete=models.CASCADE)
+    session_url = models.URLField(null=True)
+    session_id = models.CharField(max_length=255,
+                                  null=True,
+                                  blank=True)
+    money_to_pay = models.DecimalField(decimal_places=2,
+                                       max_digits=8)
