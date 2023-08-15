7a8755a7d77c72607d4a7c96cf38ff538f5bdd29
maccarets <maccarets@gmail.com>
2023-08-15 17:34:42 +0300

    add serializers for Payment model

---
+++ payment/serializer.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from borrowing.serializers import BorrowingDetailSerializer, BorrowingListSerializer
+from payment.models import Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ("id", "status", "type", "borrowing",
+                  "session_url", "session_id", "money_to_pay")
+
+class PaymentDetailSerializer(PaymentSerializer):
+    borrowing = BorrowingDetailSerializer(read_only=True, many=False)
+
+
+class PaymentListSerializer(PaymentSerializer):
+    borrowing = BorrowingListSerializer(read_only=True, many=False)
+
+
+
