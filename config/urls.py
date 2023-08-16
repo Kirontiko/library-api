6f72fecf47479e53bb74b3f938915411c74f16c0
irynamelnyk <melnyk7iryna@gmail.com>
2023-08-16 23:32:42 +0300

    Move task schedules to signals

--- config/urls.py
+++ config/urls.py
@@ -10,39 +10,3 @@ urlpatterns = [
     path("api/v1/users/", include("user.urls", namespace="user")),
     path("api/v1/borrowings/", include("borrowing.urls", namespace="borrowing")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-from django_q.tasks import schedule
-from django_q.models import Schedule
-
-if not Schedule.objects.filter(name='check borrowers').exists():
-    schedule(
-        func="notification.tasks.send_daily_notifications",
-        name="check borrowers",
-        repeats=-1,
-        schedule_type=Schedule.MINUTES,
-        minutes=10,
-    )
-
-if not Schedule.objects.filter(name='delayed borrowers').exists():
-    schedule(
-        func="notification.tasks.send_notification_delayed_return",
-        name="delayed borrowers",
-        repeats=-1,
-        schedule_type=Schedule.DAILY,
-    )
-
-if not Schedule.objects.filter(name='list of borrowers').exists():
-    schedule(
-        func="notification.tasks.send_daily_staff_notifications",
-        name="list of borrowers",
-        repeats=-1,
-        schedule_type=Schedule.DAILY,
-    )
-
-if not Schedule.objects.filter(name='remind borrowers').exists():
-    schedule(
-        func="notification.tasks.send_notification_delayed_return",
-        name="remind borrowers",
-        repeats=-1,
-        schedule_type=Schedule.DAILY,
-    )
